48f79a32733dd7b2e74c370e7333b0b5d6334beb
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2013-02-22 02:48:05 +0100

    Fix path.

--- lib/properties.rb
+++ lib/properties.rb
@@ -9,4 +9,4 @@ $NCX_TOC    = "toc.ncx"
 $OPF        = "sicm.opf"
 $LOG        = "mobi.out.txt"
 
-$KINDLE_GEN = "~/bin/kindlegen_linux_2.6_i386_v2_8/kindlegen"
+$KINDLE_GEN = "~/KindleGen/kindlegen"
