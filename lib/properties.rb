0c0da6fdd9a65d337138ace5d9d84e3343dd274d
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2013-02-22 02:03:45 +0100

    Add scripts.

---
+++ lib/properties.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'hpricot'
+
+$LOCAL_ROOT = "../content"
+$OUTPUT     = "sicm.mobi"
+
+$TOC        = "book-Z-H-4.html"
+$NCX_TOC    = "toc.ncx"
+$OPF        = "sicm.opf"
+$LOG        = "mobi.out.txt"
+
+$KINDLE_GEN = "~/bin/kindlegen_linux_2.6_i386_v2_8/kindlegen"
